3904a65d08f92eda76eaba9d5db2a08527788600
Flavio De Stefano <destefano.flavio@gmail.com>
2022-02-08 17:51:04 +0100

    Removing error in the main stack

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,16 @@
 import { DeviceOptions } from "playactor/dist/cli/options";
 const opt = new DeviceOptions();
 opt.dontAutoOpenUrls = true;
-opt.findDevice().then(async (device) => {
-  await device.openConnection();
-  console.log("Restart HomeBridge now!");
-});
+opt
+  .findDevice()
+  .then(async (device) => {
+    try {
+      await device.openConnection();
+      console.log("Restart HomeBridge now!");
+    } catch (err) {
+      // noop, as the CLI in playactor manages errors
+    }
+  })
+  .catch(() => {
+    // noop, as the CLI in playactor manages errors
+  });
