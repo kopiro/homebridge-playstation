75ed333ae0ef81f1088d55476451324b96000b33
Flavio De Stefano <destefano.flavio@gmail.com>
2022-04-26 15:01:29 +0200

    Better messaging in CLI (#35)

--- src/cli.ts
+++ src/cli.ts
@@ -4,24 +4,26 @@ import { DeviceOptions } from "playactor/dist/cli/options";
 import { Discovery } from "playactor/dist/discovery";
 import readline from "readline";
 
-const discover = async () => {
-  const discovery = new Discovery();
-  const devices = discovery.discover();
+const connect = async (deviceId) => {
+  try {
+    const opt = new DeviceOptions();
+    opt.dontAutoOpenUrls = true;
+    opt.deviceHostId = deviceId;
 
-  let success = false;
-  for await (const device of devices) {
-    console.log("Discovered device:", device);
-    const confirmed = await confirm(device.name);
-    if (confirmed) {
-      // track if there were any successful connections
-      success = (await connect(device.id)) || success;
-    }
-  }
+    console.log(`Connecting to <${deviceId}>...`);
 
-  if (success) {
-    console.log("Success, restart HomeBridge now!");
-  } else {
-    console.error("Did not authenticate to any consoles.");
+    const device = await opt.findDevice();
+    const conn = await device.openConnection();
+    console.log(
+      "Connection successful, wait a bit so we can safely close the connection..."
+    );
+    await conn.close();
+
+    return true;
+  } catch (err) {
+    const message = err instanceof Error ? err.message : err;
+    console.error(message);
+    return false;
   }
 };
 
@@ -39,22 +41,31 @@ const confirm = (deviceName) => {
   });
 };
 
-const connect = async (deviceId) => {
-  const opt = new DeviceOptions();
-  opt.dontAutoOpenUrls = true;
-  opt.deviceHostId = deviceId;
+const discover = async () => {
+  const discovery = new Discovery();
+  const devices = discovery.discover();
 
-  try {
-    const device = await opt.findDevice();
-    const conn = await device.openConnection();
-    console.log("Connection succeeded!");
-    await conn.close();
-    return true;
-  } catch (err) {
-    const message = err instanceof Error ? err.message : err;
-    console.error(message);
-    return false;
+  let success = false;
+  for await (const device of devices) {
+    console.log("Discovered device:", device);
+    const confirmed = await confirm(device.name);
+    if (confirmed) {
+      // track if there were any successful connections
+      success = (await connect(device.id)) || success;
+    }
+
+    console.log("\nDiscovering next device...");
   }
+
+  return success;
 };
 
-discover().catch((err) => console.error(err));
+discover()
+  .then((success) => {
+    if (success) {
+      console.log("\nPlease restart Homebridge now!");
+    } else {
+      console.error("\nDid not authenticate to any consoles.");
+    }
+  })
+  .catch((err) => console.error(err));
