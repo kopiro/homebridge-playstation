06d01eed8b391a31d83d6b94be3c25748f7314c0
Flavio De Stefano <destefano.flavio@gmail.com>
2024-09-08 20:27:13 +0200

    Moving to ESLINT 9

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/dist"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ),
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+];
